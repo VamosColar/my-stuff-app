c2d0c757bed82fab5487ed85013e74551636513e
Rafael Mattos <rafael_xuvisco@yahoo.com.br>
2018-03-17 21:11:37 -0300

    Adicionado migrations.

--- src/src/Migrations/Version20180318001055.php
+++ src/src/Migrations/Version20180318001055.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180317232337 extends AbstractMigration
+class Version20180318001055 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
