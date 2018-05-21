ad29290aec307b7917a5de940d0383163897686b
Rafael Mattos Conceição <rafaelconceicao@conder.intranet@1-0238.conder.intranet>
2018-05-21 17:42:37 -0300

    Videos Repositórios.

--- src/src/Repository/VideosRepository.php
+++ src/src/Repository/VideosRepository.php
@@ -3,41 +3,25 @@
 namespace App\Repository;
 
 use App\Entity\Videos;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Doctrine\RegistryInterface;
 
-class VideosRepository extends ServiceEntityRepository
+class VideosRepository
 {
     protected $entity;
 
-    public function __construct(RegistryInterface $registry)
+    public function __construct(EntityManager $entityManager)
     {
-        parent::__construct($registry, Videos::class);
+        $this->entity = $entityManager;
     }
 
     public function save(array $input)
     {
-//        $videos = new Videos();
-//        $videos->setVideTitulo($input);
-//        $this->entity = $this->getEntityManager();
-//        $this->entity->persist($videos);
-//        $this->entity->flush();
-//
-//        return $input;
+        $videos = new Videos();
+        $videos->setVideTitulo($input);
+        $this->entity->persist($videos);
+        $this->entity->flush();
+
+        return $input;
     }
 
-    /*
-    public function findBySomething($value)
-    {
-        return $this->createQueryBuilder('v')
-            ->where('v.something = :value')->setParameter('value', $value)
-            ->orderBy('v.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
 }
--- src/tests/Repository/VideosRepositoryTest.php
+++ src/tests/Repository/VideosRepositoryTest.php
@@ -9,14 +9,34 @@
 namespace tests\Repository;
 
 use App\Repository\VideosRepository;
+use Doctrine\ORM\EntityManager;
 use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class VideosRepositoryTest extends TestCase
+class VideosRepositoryTest extends KernelTestCase
 {
     protected $repository;
 
+    protected $em;
+
+    protected function setUp()
+    {
+        $kernel = self::bootKernel();
+
+        $this->em = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
     public function testInserindoDados()
     {
-//        $this->repository = new VideosRepository();
+        $input = ['dados'];
+
+        $repository = new VideosRepository($this->em);
+
+        $videos =$repository->save($input);
+
+        var_dump($videos);
     }
+
 }
